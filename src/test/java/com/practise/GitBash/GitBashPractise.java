a0b9f7839fe6dd16b1d1045f248400794527ad16
RajdipKundu1996 <rajdipkundu2006@gmail.com>
2023-10-20 18:51:28 +0530

    Commit message1 is passed by user

---
+++ src/test/java/com/practise/GitBash/GitBashPractise.java
@@ -0,0 +1,11 @@
+package com.practise.GitBash;
+
+public class GitBashPractise {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("GitBush is running. Don't Disturb");
+	}
+
+}
